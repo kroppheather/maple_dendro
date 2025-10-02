1b75ece274d5066b3d1547b2bea5bd302d20c9a4
Heather Kropp <kroppheather@gmail.com>
2025-10-02 10:39:15 -0400

    Update data_check.R

--- data_check.R
+++ data_check.R
@@ -5,6 +5,17 @@ library(ggplot2)
 dataDir <- "/Users/hkropp/Library/CloudStorage/GoogleDrive-hkropp@hamilton.edu/My Drive/research/projects/dendrometer/09_26_2025"
 files <- list.files(dataDir)
 startDay <- 256
+
+weather <- read.csv("/Users/hkropp/Library/CloudStorage/GoogleDrive-hkropp@hamilton.edu/My Drive/research/projects/dendrometer/weather/z6-10463(z6-10463)-1759414179/z6-10463(z6-10463)-Configuration 1-1759414179.1743975.csv",
+                    header=FALSE, skip=3)
+weather$date <- mdy_hms(weather$V1)
+weather$doy <- yday(weather$date)
+weather$hour <- hour(weather$date)
+hourlyWeather <- weather %>%
+  group_by(doy, hour) %>%
+  summarize(PRCP = sum(V3))
+hourlyWeather$date <- as.Date(hourlyWeather$doy-1, origin="2025-01-01")
+hourlyWeather$datetime <- ymd_hm(paste(hourlyWeather$date,hourlyWeather$hour,":00"))
 ######## sensor 1 ######
 s1 <- read.table(paste0(dataDir,"/",files[1]),header=FALSE, sep=";")
 s1$date <- ymd_hm(s1$V2)
@@ -14,7 +25,8 @@ s1$hour <- hour(s1$date)
 
 hourData <- s1 %>%
   group_by(doy,hour) %>%
-  summarise(WT = mean(V7))
+  summarise(WT = mean(V7),
+            Temp = mean(V4))
 hourData$DD <- (hourData$doy-1) + (hourData$hour/24)
 hourData <- hourData %>%
   filter(DD>=startDay)  
